71b776195d1923b162be6e8be384cb40793a0a45
hungdo <dodinhhung1601@gmail.com>
2020-05-25 16:56:20 +0700

    add other AP function

--- models/deep_learning/meals_evaluate.py
+++ models/deep_learning/meals_evaluate.py
@@ -159,6 +159,62 @@ def pred_meal(df, input_meal_arr):
     return rating
 
 
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted 
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
 def cal_recoms(pred, actual):
     '''
     Calculate recoms array
@@ -250,7 +306,8 @@ def evaluate_model(rating_arr, df_label):
             recoms = cal_recoms(top_arr[index], label_arr)
 
             # Calculate AP for each record
-            ap = cal_ap(recoms, len(label_arr))
+            # ap = cal_ap(recoms, len(label_arr))
+            ap = apk(label_arr, top_arr[index], top_k)
             ap_arr.append(ap)
 
             # Calculate Precision, Recall, AP for all data
