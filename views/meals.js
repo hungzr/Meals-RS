300237ced2c11792b3bd0e39a0b8f18220492e2f
hungzr <36071597+hungzr@users.noreply.github.com>
2020-06-09 23:00:40 +0700

    big update web

--- views/meals.js
+++ views/meals.js
@@ -1,9 +1,14 @@
 /* Set the width of the sidebar to 250px (show it) */
+
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "25%";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "30%";
+  document.getElementById("main").style.marginLeft = "30%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
