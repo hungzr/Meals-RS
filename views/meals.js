6e6ee2d6dc10384632d78d806fa550bd64d14062
hungdo <dodinhhung1601@gmail.com>
2020-06-10 17:24:31 +0700

    update web

--- views/meals.js
+++ views/meals.js
@@ -1,14 +1,17 @@
 /* Set the width of the sidebar to 250px (show it) */
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "30%";
-  document.getElementById("main").style.marginLeft = "30%";
+  document.getElementById("mySidenav").style.width = "25%";
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("option-sign").style.marginLeft = "-18%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("option-sign").style.marginLeft= "auto";
+  document.getElementById("option-sign").style.marginRight= "0";
   document.body.style.backgroundColor = "white";
 }
 
