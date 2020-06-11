1e9e82bf59ac00164c07af1f6a47c5442e3c3abe
hungzr <36071597+hungzr@users.noreply.github.com>
2020-06-11 23:28:53 +0700

    add detail web

--- views/meals.js
+++ views/meals.js
@@ -1,9 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "25%";
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("option-sign").style.marginLeft = "-18%";
+  document.getElementById("mySidenav").style.width = "30%";
+  document.getElementById("main").style.marginLeft = "30%";
+  document.getElementById("option-sign").style.marginLeft = "-22%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
@@ -15,3 +15,15 @@ function closeNav() {
   document.body.style.backgroundColor = "white";
 }
 
+// Rating Initialization
+$(document).ready(function() {
+  $('#rateMe4').mdbRate();
+});
+
+
+$(function () {
+    $('.selectpicker').selectpicker();
+});
+
+// Animations initialization
+new WOW().init();
